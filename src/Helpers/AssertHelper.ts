a2ef478f9d5069bf97da79369623eed70beba49c
SIEGE\S832491 <johnny.behague.umanis@axa.fr>
2018-07-13 10:41:06 +0200

    migrate tests

--- src/Helpers/AssertHelper.ts
+++ src/Helpers/AssertHelper.ts
@@ -1,24 +1,60 @@
 'use strict';
 
-import { ResponseEnvelope, ui } from 'ask-sdk-model'
+import { ResponseEnvelope, Response, ui } from 'ask-sdk-model'
 import { expect } from 'chai';
 
 export default class AssertHelper {
-    checkResponseStructure(response: ResponseEnvelope): void {
-        expect(response).to.have.property("version");
-        expect(response.version).to.be.equal("1.0");
-        expect(response).to.have.property("response");
+    getResponseFromResponseEnvelope(responseEnvelope: ResponseEnvelope): Response {
+        return responseEnvelope.response;
     }
 
-    checkOutputSpeech(response: ResponseEnvelope): void {
+    getSsmlOutputSpeechFromResponse(response: Response): ui.SsmlOutputSpeech {
+        return <ui.SsmlOutputSpeech>response.outputSpeech;
+    }
 
-        expect(response).to.have.property("response");
-        let r = response.response;
+    getSsmlTextFromSsmlOutputSpeech(ssmlOutputSpeech: ui.SsmlOutputSpeech): string {
+        return ssmlOutputSpeech.ssml;
+    }
 
-        expect(r).to.have.property("outputSpeech");
-        expect(r.outputSpeech).to.have.property("type");
-        expect(r.outputSpeech.type).to.equal('SSML');
-        expect(r.outputSpeech).to.have.property("ssml");
+    getSessionStatus(response: Response): boolean {
+        return response.shouldEndSession;
+    }
+
+    getStandardCard(response: Response): ui.StandardCard {
+       let card: ui.StandardCard;
+
+       if(response.card.type === 'Standard') {
+           card = <ui.StandardCard>response.card;
+       }
+
+       return card;
+    }
+
+
+    // private checkResponseStructureAndGetResponse(responseEnvelope: ResponseEnvelope): Response {
+    //     expect(responseEnvelope).to.have.property("version");
+    //     expect(responseEnvelope.version).to.be.equal("1.0");
+    //     expect(responseEnvelope).to.have.property("response");
+
+    //     return responseEnvelope.response;
+    // }
+
+    // private checkOutputSpeechAndGetSsmlOutputSpeech(response: Response): ui.SsmlOutputSpeech {
+    //     expect(response).to.have.property("outputSpeech");
+    //     expect(response.outputSpeech).to.have.property("type");
+    //     expect(response.outputSpeech.type).to.equal('SSML');
+    //     expect(response.outputSpeech).to.have.property("ssml");
+
+    //     let ssmlOutputSpeech = <ui.SsmlOutputSpeech>response.outputSpeech;
+
+    //     expect(ssmlOutputSpeech.ssml).to.match(/^<speak>/);
+    //     expect(ssmlOutputSpeech.ssml).to.match(/<\/speak>$/);
+
+    //     return <ui.SsmlOutputSpeech>ssmlOutputSpeech;
+    // }
+
+    checkOutputSpeechSsmlStructure(response: ResponseEnvelope): void {
+        this.checkSsmlOutputSpeech(response);
 
         let os = <ui.SsmlOutputSpeech>r.outputSpeech;
 
---
+++ src/Helpers/ResponseHelper.ts
@@ -0,0 +1,56 @@
+'use strict';
+
+import { ResponseEnvelope, Response, ui } from 'ask-sdk-model'
+
+export default class ResponseHelper {
+    getResponseFromResponseEnvelope(responseEnvelope: ResponseEnvelope): Response {
+        return responseEnvelope.response;
+    }
+
+    getSsmlOutputSpeechFromResponse(response: Response): ui.SsmlOutputSpeech {
+        return <ui.SsmlOutputSpeech>response.outputSpeech;
+    }
+
+    getSsmlOutputSpeechFromResponseEnvelope(responseEnvelope: ResponseEnvelope): ui.SsmlOutputSpeech {
+        const response = this.getResponseFromResponseEnvelope(responseEnvelope);
+        return this.getSsmlOutputSpeechFromResponse(response);
+    }
+
+    getSsmlTextFromSsmlOutputSpeech(ssmlOutputSpeech: ui.SsmlOutputSpeech): string {
+        return ssmlOutputSpeech.ssml;
+    }
+
+    getSsmlTextFromResponse(response: Response): string {
+        const ssmlOutputSpeech = this.getSsmlOutputSpeechFromResponse(response);
+        return this.getSsmlTextFromSsmlOutputSpeech(ssmlOutputSpeech);
+    }
+
+    getSsmlTextFromResponseEnvelope(responseEnvelope: ResponseEnvelope): string {
+        const ssmlOutputSpeech = this.getSsmlOutputSpeechFromResponseEnvelope(responseEnvelope);
+        return this.getSsmlTextFromSsmlOutputSpeech(ssmlOutputSpeech);
+    }
+
+    getSessionStatusFromResponse(response: Response): boolean {
+        return response.shouldEndSession;
+    }
+
+    getSessionStatusFromResponseEnvelope(responseEnvelope: ResponseEnvelope): boolean {
+        const response = this.getResponseFromResponseEnvelope(responseEnvelope);
+        return this.getSessionStatusFromResponse(response);
+    }
+
+    getStandardCardFromResponse(response: Response): ui.StandardCard {
+       let card: ui.StandardCard;
+
+       if(response.card.type === 'Standard') {
+           card = <ui.StandardCard>response.card;
+       }
+
+       return card;
+    }
+
+    getStandardCardFromResponseEnvelope(responseEnvelope: ResponseEnvelope): ui.StandardCard {
+       const response = this.getResponseFromResponseEnvelope(responseEnvelope);
+       return this.getStandardCardFromResponse(response);
+    }
+};
